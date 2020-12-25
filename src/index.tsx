21bbc3a0c47cc80de4188671841950f9d7b43308
杨广 <yangguang@forchange.tech>
2020-12-25 17:51:10 +0800

    rename file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Cart from './cart'
+import 'antd/dist/antd.css';
+import styled from "styled-components";
+
+const AppDiv = styled.div`
+    text-align: center;
+`
+
+function App() {
+    return (
+        <AppDiv>
+            <Cart/>
+        </AppDiv>
+    );
+}
 
 ReactDOM.render(<App/>, document.querySelector('#app'));
 
