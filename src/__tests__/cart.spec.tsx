67888743c9306cc2e7f382dbf797d25c91ea2ec5
杨广 <yangguang@forchange.tech>
2020-12-28 16:06:27 +0800

    shallow mount test

--- src/__tests__/cart.spec.tsx
+++ src/__tests__/cart.spec.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Cart from '../cart/index';
-import {mount} from 'enzyme';
+import CartItem from '../cart/CartItem';
+import {mount, shallow} from 'enzyme';
 
 describe('CartTest', () => {
-    it('renders without crashing', () => {
+    it('shallow', () => {
+        const wrapper = shallow(<Cart/>);
+        expect(wrapper.find(CartItem).length).toBe(0);
+    });
+    it('mount', () => {
         const wrapper = mount(<Cart/>);
-        expect(wrapper.find('.ant-list-item').length).toBe(5);
+        expect(wrapper.find(CartItem).length).toBe(5);
     });
 });
